6b2c8d0c77397510f6dc912416981b4056f7db9a
saidelhadad860-byte <saidelhadad860@gmail.com>
2026-04-20 12:35:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".item");
+
+  items.forEach((item, i) => {
+    item.style.opacity = "0";
+    item.style.transform = "translateY(20px)";
+
+    setTimeout(() => {
+      item.style.transition = "0.5s";
+      item.style.opacity = "1";
+      item.style.transform = "translateY(0)";
+    }, i * 100);
+  });
+});
